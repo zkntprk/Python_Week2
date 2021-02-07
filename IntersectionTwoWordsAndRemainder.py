e88d5c9a7cf592a61d003e19ae19a474d8446c71
özkan <58474594+zkntprk@users.noreply.github.com>
2021-02-08 00:12:55 +0200

    Ozkan (Week 2 Homeworks)

---
+++ IntersectionTwoWordsAndRemainder.py
@@ -0,0 +1,43 @@
+### All Rights Reserved ###
+
+#Copyright (c) ${IntersectionTwoWordsAndReminder.2021} ${Ozkan TOPRAK}
+
+#Created by InfotecAcademy
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+
+word1=set(input('Please enter your first word without any punctuation and space: '))
+word2=set(input('Please enter your second word without any punctuation and space: '))
+
+a=(sorted(list(word1.intersection(word2))))
+a[:] = [''.join(a[:])] 
+
+b=(sorted(list(word1.difference(word2))))
+b[:] = [''.join(b[:])] 
+
+c=(sorted(list(word2.difference(word1))))
+c[:] = [''.join(c[:])] 
+
+d=[a[0],b[0],c[0]]
+print('Solution (Intersection/difference1/difference2): ',d )
+
+
+### All Rights Reserved ###
+
+#Copyright (c) ${IntersectionTwoWordsAndReminder.2021} ${Ozkan TOPRAK}
+
+#Created by InfotecAcademy
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
